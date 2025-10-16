8ddfb79a81002b74c0b4ac74925f3f7dc2db33f3
vasqujen <jvasqu2877@gmail.com>
2025-10-16 12:30:19 -0400

    initial commit

---
+++ app/src/main/java/com/example/gmailapp/Email.kt
@@ -0,0 +1,7 @@
+package com.example.gmailapp
+
+class Email(
+    val sender: String,
+    val title: String,
+    val summary: String) {
+}
---
+++ app/src/main/java/com/example/gmailapp/EmailAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.gmailapp
+
+import android.media.tv.TvView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
+
+class EmailAdapter(private val emails: List<Email>) : RecyclerView.Adapter<EmailAdapter.ViewHolder>(){
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        // Inflate the custom layout
+        val contactView = inflater.inflate(R.layout.email_item, parent, false)
+        // Return a new holder instance
+        return ViewHolder(contactView)
+    }
+
+    override fun onBindViewHolder(
+        holder: ViewHolder,
+        position: Int
+    ) {
+        // Get the data model based on position
+        val email = emails.get(position)
+        // Set item views based on views and data model
+        holder.senderTextView.text = email.sender
+        holder.titleTextView.text = email.title
+        holder.summaryTextView.text = email.summary
+    }
+
+    override fun getItemCount(): Int {
+        return emails.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        val senderTextView: TextView
+        val titleTextView: TextView
+        val summaryTextView: TextView
+
+        init {
+            senderTextView = itemView.findViewById<TextView>(R.id.senderTv)
+            titleTextView = itemView.findViewById<TextView>(R.id.titleTv)
+            summaryTextView = itemView.findViewById<TextView>(R.id.summaryTv)
+        }
+    }
+}
